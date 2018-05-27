facc5a46d754050a58c0974ea29fbff4818b2401
Matt <matthewprimpas@gmail.com>
2018-05-26 21:06:02 -0400

    Migrate User table to database

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180526005659) do
+ActiveRecord::Schema.define(version: 20180527010438) do
 
   create_table "messages", force: :cascade do |t|
     t.text "contents"
@@ -21,6 +21,13 @@ ActiveRecord::Schema.define(version: 20180526005659) do
   create_table "rooms", force: :cascade do |t|
     t.string "name"
     t.integer "messages"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "first_name"
+    t.string "last_name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
